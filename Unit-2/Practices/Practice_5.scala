7d51ae2bd28b7e08d6da4db2d22e4ad01c25ea27
seashelltec <62223929+seashelltec@users.noreply.github.com>
2021-11-15 21:40:59 -0800

    Finish the practice 5 only code

--- Unit-2/Practices/Practice_5.scala
+++ Unit-2/Practices/Practice_5.scala
@@ -4,4 +4,20 @@ import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 // Load the data stored in LIBSVM format as a DataFrame.
 val data = spark.read.format("libsvm").load("SampleLivs.txt")
 
+
+val splits = data.randomSplit(Array(0.6, 0.4), seed = 1503L)
 val train = splits(0)
+val test = splits(1)
+val layers = Array[Int](8, 5, 4, 8)
+
+
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(130).setSeed(1503L).setMaxIter(100)
+val model = trainer.fit(train)
+
+val result = model.transform(test)
+val predictLabels = result.select("prediction", "label")
+val eval = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+
+
+
+println(s"Test:  ${eval.evaluate(predictLabels)}")
